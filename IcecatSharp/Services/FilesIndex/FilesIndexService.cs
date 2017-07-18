8f41dd2d3732e23b997590a0818b310905d44d77
vinhch <vinhomn@gmail.com>
2017-07-18 23:46:52 +0700

    add FilesIndexService

---
+++ IcecatSharp/Services/FilesIndex/FilesIndexService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using IcecatSharp.Infrastructure;
+using IcecatSharp.Models;
+
+namespace IcecatSharp.Services
+{
+    public class FilesIndexService : BaseService
+    {
+        public FilesIndexService(IceCatAccessConfig config) : base(config)
+        {
+            XmlFileName = "files.index.xml.gz";
+        }
+
+        public async Task<string> DownloadAndExtractAsync()
+        {
+            var req = RequestEngine.CreateClient(_AccessConfig);
+
+            var gzipFilePath = await RequestEngine.DownloadFileAsync(req, XmlFileUrl, _AccessConfig.DownloadDirectory);
+
+            var unzipFilePath = GZipUtils.Decompress(new FileInfo(gzipFilePath));
+
+            return unzipFilePath;
+        }
+
+        public IEnumerable<IceCatFile> List()
+        {
+            return CustomXmlParser.ParseFileToList<IceCatFile>(XmlFilePath, "file");
+        }
+    }
+}
