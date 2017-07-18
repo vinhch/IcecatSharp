dd7e91f9dc2d5e8b965c78169e4ae761f28bc74f
vinhch <vinhomn@gmail.com>
2017-07-18 23:03:20 +0700

    update RequestEngine

--- IcecatSharp/Infrastructure/RequestEngine.cs
+++ IcecatSharp/Infrastructure/RequestEngine.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace IcecatSharp.Infrastructure
 {
@@ -34,6 +35,82 @@ namespace IcecatSharp.Infrastructure
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
 
             return client;
+        }
+
+        public static async Task<string> DownloadFileAsync(HttpClient client, string downloadUrl, string saveFilePath)
+        {
+            var saveDirectoryPath = Path.GetDirectoryName(saveFilePath);
+            CreateFolderIfNeeded(saveDirectoryPath);
+
+            var fileName = saveFilePath.Split('\\').Last().ToLower().Trim();
+            fileName = GetValidFileName(fileName);
+
+            var filePath = $"{saveDirectoryPath}\\{fileName}";
+
+            using (var response = await client.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead)
+                .ConfigureAwait(false))
+            {
+                response.EnsureSuccessStatusCode();
+                using (var contentStream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                {
+                    //Response.BufferOutput = false;   // to prevent buffering
+                    //var totalRead = 0L;
+                    //var totalReads = 0L;
+                    var buffer = new byte[8192];
+                    var isMoreToRead = true;
+
+                    do
+                    {
+                        var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                        if (read == 0)
+                        {
+                            isMoreToRead = false;
+                        }
+                        else
+                        {
+                            await fileStream.WriteAsync(buffer, 0, read);
+
+                            //totalRead += read;
+                            //totalReads += 1;
+                            //if (totalReads % 2000 == 0)
+                            //{
+                            //    Console.WriteLine(string.Format("total bytes downloaded so far: {0:n0}", totalRead));
+                            //}
+                        }
+                    }
+                    while (isMoreToRead);
+                }
+            }
+
+            return filePath;
+        }
+
+        public static Task<string> DownloadFileToDirectoryAsync(HttpClient client, string downloadUrl, string saveDirectoryPath)
+        {
+            var fileName = downloadUrl.Split('/').Last().ToLower().Trim();
+
+            var filePath = $"{saveDirectoryPath}\\{fileName}";
+
+            return DownloadFileAsync(client, downloadUrl, filePath);
+        }
+
+        public static async Task<byte[]> GetAsByteAsync(HttpClient client, string url)
+        {
+            using (var response = await client.GetAsync(url).ConfigureAwait(false))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsByteArrayAsync();
+            }
+        }
+
+        public static async Task<string> GetAsStringAsync(HttpClient client, string url)
+        {
+            using (var response = await client.GetAsync(url).ConfigureAwait(false))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
         }
     }
 }
