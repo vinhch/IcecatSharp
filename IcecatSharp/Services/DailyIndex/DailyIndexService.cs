000d5f757a64759a3ae69813871377f4adaefff0
vinhch <vinhomn@gmail.com>
2017-07-19 23:32:04 +0700

    add DailyIndexService

---
+++ IcecatSharp/Services/DailyIndex/DailyIndexService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using IcecatSharp.Infrastructure;
+using IcecatSharp.Models;
+
+namespace IcecatSharp.Services
+{
+    public class DailyIndexService : BaseService
+    {
+        public DailyIndexService(IceCatAccessConfig config) : base(config)
+        {
+            XmlFileName = "daily.index.xml.gz";
+        }
+
+        public async Task<string> DownloadAndExtractAsync()
+        {
+            var req = RequestEngine.CreateClient(_AccessConfig);
+
+            var gzipFilePath = await RequestEngine.DownloadFileToDirectoryAsync(req, XmlFileUrl, _AccessConfig.DownloadDirectory);
+
+            var unzipFilePath = await GZipUtils.DecompressAsync(new FileInfo(gzipFilePath));
+
+            return unzipFilePath;
+        }
+
+        public IEnumerable<IceCatFile> List()
+        {
+            return CustomXmlParser.ParseFileToList<IceCatFile>(XmlFilePath, "file");
+        }
+    }
+}
