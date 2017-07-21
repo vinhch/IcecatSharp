373368585955171e0dcdea74a8a6b0c743aae26f
vinhch <vinhomn@gmail.com>
2017-07-22 01:07:12 +0700

    add FilesIndex Tests

---
+++ test/IcecatSharp.Tests/FilesIndex/FilesIndex_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IcecatSharp.Tests
+{
+    [Trait("Category", "FilesIndex")]
+    public class FilesIndex_Tests : IClassFixture<FilesIndex_Tests_Fixture>
+    {
+        private readonly FilesIndex_Tests_Fixture _fixture;
+
+        public FilesIndex_Tests(FilesIndex_Tests_Fixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task DownloadAndExtract_FilesIndex()
+        {
+            var filePath = await _fixture.Service.DownloadAndExtractAsync();
+            Assert.True(File.Exists(filePath));
+        }
+
+        [Fact]
+        public async Task List_FilesIndex()
+        {
+            var icecatFiles = _fixture.Service.List();
+            var count = icecatFiles.Count();
+            Assert.True(count > 0);
+        }
+    }
+}
---
+++ test/IcecatSharp.Tests/FilesIndex/FilesIndex_Tests_Fixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using IcecatSharp.Services;
+using Xunit;
+
+namespace IcecatSharp.Tests
+{
+    public class FilesIndex_Tests_Fixture : IAsyncLifetime
+    {
+        public FilesIndexService Service => new FilesIndexService(Utils.IceCatConfig);
+
+        public async Task InitializeAsync()
+        {
+        }
+
+        public async Task DisposeAsync()
+        {
+        }
+    }
+}
